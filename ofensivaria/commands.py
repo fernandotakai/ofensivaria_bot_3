c2d0a622dce7b9cb044f482aaca383886a4e5087
Fernando Takai <fernando.takai@gmail.com>
2017-01-17 18:56:39 -0200

    can't square if it's less than two

--- ofensivaria/commands.py
+++ ofensivaria/commands.py
@@ -496,4 +496,6 @@ class SquareMeme(Command):
     async def respond(self, text, message):
         text = message['args']['text']
         text = text.replace(' ', '')
+        if text < 2:
+            return False
         return '```\n%s\n```' % self.meme(text)
